ddf8dd3fde1d0d4eec31baa2143d6973945b7507
Aditya Kakad <202052302@iiitvadodara.ac.in>
2022-08-10 01:26:39 +0530

    yugwdhjv

---
+++ Data Structures/Arrays/LargestContiguousSum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class LargestContiguousSum {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(largestsum(arr));
+        sc.close();
+    }
+    static int largestsum(int[] arr) {
+        
+        return 0;
+    }
+}
